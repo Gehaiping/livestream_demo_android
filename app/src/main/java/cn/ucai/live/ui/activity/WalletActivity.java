4edd8ef198175e0a04c10db67d0d21cf30d89f5e
gehaiping <804563299@qq.com>
2017-03-06 14:05:32 +0800

    我的钱包界面

--- app/src/main/java/cn/ucai/live/ui/activity/WalletActivity.java
+++ app/src/main/java/cn/ucai/live/ui/activity/WalletActivity.java
@@ -2,15 +2,30 @@ package cn.ucai.live.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.live.R;
 
 
 public class WalletActivity extends AppCompatActivity {
 
+    @BindView(R.id.tv_change_balance)
+    TextView mTvChangeBalance;
+    @BindView(R.id.target_layout)
+    LinearLayout mTargetLayout;
+    View loadingView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallet);
+        ButterKnife.bind(this);
+        loadingView = LayoutInflater.from(WalletActivity.this).inflate(R.layout.rp_loading, mTargetLayout, false);
+        mTargetLayout.addView(loadingView);
     }
 }
